44f3e8292c7935fca8a0867dd57aabc34010c7f0
Ivan Ajenjo <ivan.ajenjo.vicente@gmail.com>
2020-03-24 12:56:46 +0100

    init Commit

--- build/main.d
+++ build/main.d
@@ -1,4 +1,7 @@
 main.o: /home/ivan/Escritorio/The-Binding-of-Isaac/source/main.c \
+ /opt/devkitpro/libctru/include/citro2d.h \
+ /opt/devkitpro/libctru/include/citro3d.h \
+ /opt/devkitpro/libctru/include/c3d/types.h \
  /opt/devkitpro/libctru/include/3ds.h \
  /opt/devkitpro/libctru/include/3ds/types.h \
  /opt/devkitpro/libctru/include/3ds/result.h \
@@ -78,7 +81,34 @@ main.o: /home/ivan/Escritorio/The-Binding-of-Isaac/source/main.c \
  /opt/devkitpro/libctru/include/3ds/sdmc.h \
  /opt/devkitpro/libctru/include/3ds/romfs.h \
  /opt/devkitpro/libctru/include/3ds/font.h \
- /opt/devkitpro/libctru/include/3ds/gdbhio_dev.h
+ /opt/devkitpro/libctru/include/3ds/gdbhio_dev.h \
+ /opt/devkitpro/libctru/include/c3d/maths.h \
+ /opt/devkitpro/libctru/include/c3d/mtxstack.h \
+ /opt/devkitpro/libctru/include/c3d/uniforms.h \
+ /opt/devkitpro/libctru/include/c3d/attribs.h \
+ /opt/devkitpro/libctru/include/c3d/buffers.h \
+ /opt/devkitpro/libctru/include/c3d/base.h \
+ /opt/devkitpro/libctru/include/c3d/texenv.h \
+ /opt/devkitpro/libctru/include/c3d/effect.h \
+ /opt/devkitpro/libctru/include/c3d/texture.h \
+ /opt/devkitpro/libctru/include/c3d/proctex.h \
+ /opt/devkitpro/libctru/include/c3d/light.h \
+ /opt/devkitpro/libctru/include/c3d/lightlut.h \
+ /opt/devkitpro/libctru/include/c3d/fog.h \
+ /opt/devkitpro/libctru/include/c3d/framebuffer.h \
+ /opt/devkitpro/libctru/include/c3d/renderqueue.h \
+ /opt/devkitpro/libctru/include/tex3ds.h \
+ /opt/devkitpro/libctru/include/c2d/base.h \
+ /opt/devkitpro/libctru/include/c2d/spritesheet.h \
+ /opt/devkitpro/libctru/include/c2d/sprite.h \
+ /opt/devkitpro/libctru/include/c2d/text.h \
+ /opt/devkitpro/libctru/include/c2d/font.h
+
+/opt/devkitpro/libctru/include/citro2d.h:
+
+/opt/devkitpro/libctru/include/citro3d.h:
+
+/opt/devkitpro/libctru/include/c3d/types.h:
 
 /opt/devkitpro/libctru/include/3ds.h:
 
@@ -239,3 +269,45 @@ main.o: /home/ivan/Escritorio/The-Binding-of-Isaac/source/main.c \
 /opt/devkitpro/libctru/include/3ds/font.h:
 
 /opt/devkitpro/libctru/include/3ds/gdbhio_dev.h:
+
+/opt/devkitpro/libctru/include/c3d/maths.h:
+
+/opt/devkitpro/libctru/include/c3d/mtxstack.h:
+
+/opt/devkitpro/libctru/include/c3d/uniforms.h:
+
+/opt/devkitpro/libctru/include/c3d/attribs.h:
+
+/opt/devkitpro/libctru/include/c3d/buffers.h:
+
+/opt/devkitpro/libctru/include/c3d/base.h:
+
+/opt/devkitpro/libctru/include/c3d/texenv.h:
+
+/opt/devkitpro/libctru/include/c3d/effect.h:
+
+/opt/devkitpro/libctru/include/c3d/texture.h:
+
+/opt/devkitpro/libctru/include/c3d/proctex.h:
+
+/opt/devkitpro/libctru/include/c3d/light.h:
+
+/opt/devkitpro/libctru/include/c3d/lightlut.h:
+
+/opt/devkitpro/libctru/include/c3d/fog.h:
+
+/opt/devkitpro/libctru/include/c3d/framebuffer.h:
+
+/opt/devkitpro/libctru/include/c3d/renderqueue.h:
+
+/opt/devkitpro/libctru/include/tex3ds.h:
+
+/opt/devkitpro/libctru/include/c2d/base.h:
+
+/opt/devkitpro/libctru/include/c2d/spritesheet.h:
+
+/opt/devkitpro/libctru/include/c2d/sprite.h:
+
+/opt/devkitpro/libctru/include/c2d/text.h:
+
+/opt/devkitpro/libctru/include/c2d/font.h:
