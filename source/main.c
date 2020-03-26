944ccb0aefeddf22394930e0c32f30cd5186543e
Ivan Ajenjo <ivan.ajenjo.vicente@gmail.com>
2020-03-26 20:22:10 +0100

    commit

--- source/main.c
+++ source/main.c
@@ -81,15 +81,15 @@ static void moveLeft(){
 }
 //Logica del disparo del personaje
 static void shootUp(){
-	C2D_SpriteSetRotation(&mainCharacter.spr, C3D_Angle(0.25f));
-}
-
-static void shootDown(){
 	C2D_SpriteSetRotation(&mainCharacter.spr, C3D_Angle(0.0f));
 }
 
-static void shootRight(){
+static void shootDown(){
 	C2D_SpriteSetRotation(&mainCharacter.spr, C3D_Angle(0.5f));
+}
+
+static void shootRight(){
+	C2D_SpriteSetRotation(&mainCharacter.spr, C3D_Angle(0.25f));
 }
 
 static void shootLeft(){
