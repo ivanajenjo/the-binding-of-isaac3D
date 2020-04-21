412cf053559317c15b7c1078548126148566afb0
Ivan Ajenjo <ivan.ajenjo.vicente@gmail.com>
2020-04-21 12:39:10 +0200

    commit

--- source/main.c
+++ source/main.c
@@ -481,7 +481,7 @@ static void checkCollisions()
 		{
 			if (deathHeads[j].visible == true)
 			{
-				if ((abs(disparos[i].spr.params.pos.x - deathHeads[j].spr.params.pos.x) < 50) && (abs(disparos[i].spr.params.pos.y - deathHeads[j].spr.params.pos.y) < 50))
+				if ((abs(disparos[i].spr.params.pos.x - deathHeads[j].spr.params.pos.x) < 10) && (abs(disparos[i].spr.params.pos.y - deathHeads[j].spr.params.pos.y) < 10))
 				{
 					disparos[i].visible = false;
 					deathHeads[j].visible = false;
